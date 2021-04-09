c2a71ee444ba9a7f27381d624fa22df545854339
Andrea Esposito <andexpox@gmail.com>
2021-04-09 21:16:56 +0200

    chevron-up-down

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 $(document).ready(function() {
     $('.li-dropdown').mouseenter(function() {
         $(this).children('.dropdown').addClass('active');
+        $('.fas').removeClass('fa-chevron-down').addClass('fa-chevron-up');
     });
 
     $('.li-dropdown').mouseleave(function() {
         $(this).children('.dropdown').removeClass('active');
+        $('.fas').removeClass('fa-chevron-up').addClass('fa-chevron-down');
     });
+
 });
