1d7346e3de533e58e837fe20081b510bbfdde236
Andrea Esposito <andexpox@gmail.com>
2021-04-09 20:52:25 +0200

    wip-css-js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 //
+$(document).ready(function() {
+    $('.li-dropdown').mouseenter(function() {
+        $(this).children('.dropdown').addClass('active');
+    });
+
+    $('.li-dropdown').mouseleave(function() {
+        $(this).children('.dropdown').removeClass('active');
+    });
+});
